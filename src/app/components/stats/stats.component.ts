830533614a9ddb72f421f4c97b817c24bd33c477
AJVelezRueda <anavelezrueda@gmail.com>
2018-11-08 19:18:16 -0300

    Ultimos detalles, correccion de texto y estadisticas

--- src/app/components/stats/stats.component.ts
+++ src/app/components/stats/stats.component.ts
@@ -42,17 +42,17 @@ export class StatsComponent implements OnInit {
           {"label": "Amycolatopsis sp.","value": "1.05"},
           {"label": "Pseudomonas putida","value": "2.10"},
           {"label": "Thermomyces lanuginosus (Humicola lanuginosa)","value": "2.10"},
-          {"label": "Pseudomonas putida (Arthrobacter siderocapsulatus)","value": "6.31"},
+          {"label": "Pseudomonas putida (Arthrobacter siderocapsulatus)","value": "3.29"},
           {"label": "Rhodococcus erythropolis (Arthrobacter picolinophilus)","value": "1.05"},
           {"label": "Neosartorya fumigata","value": "3.15"},
-          {"label": "Rhizopus delemar","value": "2.10"},
+          {"label": "Rhizopus delemar","value": "1.09"},
           {"label": "Ficus carica","value": "1.05"},
           {"label": "Escherichia coli (strain K12)","value": "1.05"},
           {"label": "Pseudozyma antarctica (Candida antarctica)","value": "4.21"},
           {"label": "Bacillus licheniformis","value": "3.15789473684"},
           {"label": "Burkholderia glumae (Pseudomonas glumae)","value": "1.05"},
           {"label": "Pseudomonas chlororaphis","value": "1.05"},
-          {"label": "Homo sapiens (Human)","value": "5.26"},
+          {"label": "Homo sapiens (Human)","color": "#bdbdbd","value": "8.79"},
           {"label": "Methylomonas aminofaciens","value": "1.05"},
           {"label": "Salmonella typhimurium (strain LT2 / SGSC1412 / ATCC 700720)","value": "1.05"},
           {"label": "Aspergillus usamii","value": "1.05"},
@@ -61,22 +61,21 @@ export class StatsComponent implements OnInit {
           {"label": "Acinetobacter johnsonii","value": "1.05"},
           {"label": "Brevundimonas diminuta (Pseudomonas diminuta)","value": "2.10"},
           {"label": "Rhizopus niveus","value": "2.10526315789"},
-          {"label": "Bos taurus (Bovine)","color": "#bdbdbd","value": "10.5"},
+          {"label": "Bos taurus (Bovine)","color": "#bdbdbd","value": "8.795"},
           {"label": "Pseudomonas aeruginosa","value": "1.05"},
           {"label": "Citrobacter freundii","value": "1.05"},
           {"label": "Aspergillus niger","value": "2.10"},
           {"label": "Carica papaya (Papaya)","value": "1.05"},
           {"label": "Arthrobacter sp.","value": "1.05"},
           {"label": "Thermomyces lanuginosus","value": "1.05"},
-          {"label": "Bacillus subtilis","value": "2.10"},
-          {"label": "Escherichia coli","value": "8.42"},
+          {"label": "Bacillus subtilis","value": "1.09"},
+          {"label": "Escherichia coli","color": "#bdbdbd","value": "8.42"},
           {"label": "Macrophoma commelinae","value": "2.10"},
-          {"label": "Sus scrofa (Pig)","color": "#bdbdbd","value": "10.5"},
+          {"label": "Sus scrofa (Pig)","color": "#bdbdbd","value": "12.08"},
           {"label": "Pseudomonas fluorescens","value": "1.05"},
-          {"label": "Human","value": "3.15"},
           {"label": "Microbacterium hydrocarbonoxydans","value": "1.05"},
-          {"label": "Rhizomucor miehei","value": "1.05263157895"},
-          {"label": "Pseudozyma antarctica (Yeast) (Candida antarctica)","value": "1.052"}
+          {"label": "Rhizomucor miehei","value": "2.1993157895"},
+          {"label": "Pseudozyma antarctica (Yeast) (Candida antarctica)","value": "3.296"}
         ]
       }
     }
@@ -105,15 +104,19 @@ export class StatsComponent implements OnInit {
           {
               "label": "AB hydrolase superfamily",
               "color": "#bdbdbd",
-              "value": "9.80"
+              "value": "11.90"
           },
           {
               "label": "Glycosyl hydrolase 22 family",
-              "value": "1.96"
+              "value": "2.38"
+          },
+          {
+              "label": "Globin family",
+              "value": "4.76"
           },
           {
               "label": "Paraoxonase family",
-              "value": "1.96"
+              "value": "2.38"
           },
           {
               "label": "Phosphotriesterase family",
@@ -121,60 +124,60 @@ export class StatsComponent implements OnInit {
           },
           {
               "label": "Peptidase M20A family DapE subfamily",
-              "value": "1.96"
+              "value": "2.38"
           },
           {
-              "label": "Tryptophan dimethylallyltransferase",
-              "value": "5.88"
+              "label": "Tryptophan dimethylallyltransferase family",
+              "color": "#bdbdbd",
+              "value": "7.14"
           },
           {
               "label": "Glycosyl hydrolase 15",
-              "value": "1.96"
+              "value": "2.38"
           },
           {
               "label": "Alpha-carbonic anhydrase",
-              "value": "1.96"
+              "value": "2.38"
           },
           {
               "label": "Glycosyl hydrolase 13",
-              "value": "3.96"
+              "value": "4.76"
           },
           {
               "label": "Sulfatase",
-              "value": "1.96"
+              "value": "2.38"
           },
           {
               "label": "Trans-sulfuration enzymes",
-              "value": "1.96"
+              "value": "2.38"
           },
           {
               "label": "ALB/AFP/VDB",
-              "value": "3.96"
+              "value": "4.76"
           },
           {
               "label": "HPS/KGPDC family HPS",
-              "value": "1.96"
+              "value": "2.38"
           },
           {
               "label": "'GDSL' lipolytic enzyme",
-              "value": "1.96"
+              "value": "2.38"
           },
           {
-              "label": "'Globin",
-              "value": "3.96"
+              "label": "'Peptidase M20A family",
+              "value": "2.38"
           },
           {
               "label": "4-oxalocrotonate tautomerase",
-              "color": "#bdbdbd",
-              "value": "7.80"
+              "value": "2.38"
           },
           {
               "label": "HPS/KGPDC family KGPDC",
-              "value": "1.96"
+              "value": "2.38"
           },
           {
               "label": "Peptidase A1",
-              "value": "1.96"
+              "value": "2.38"
           },
           {
               "label": "AB hydrolase",
@@ -182,27 +185,28 @@ export class StatsComponent implements OnInit {
           },
           {
               "label": "Peptidase S8",
-              "value": "5.88"
+              "value": "2.38"
           },
           {
               "label": "Peptidase S1",
-              "value": "3.96"
+              "value": "4.76"
           },
           {
-              "label": "Peptidase C1",
-              "value": "5.88"
+              "label": "Peptidase C1 family",
+              "color": "#bdbdbd",
+              "value": "7.14"
           },
           {
               "label": "Nuclease type I",
-              "value": "1.96"
+              "value": "2.38"
           },
           {
               "label": "Mandelate racemase/muconate lactonizing",
-              "value": "3.96"
+              "value": "2.38"
           },
           {
               "label": "Metallo-dependent hydrolase",
-              "value": "3.96"
+              "value": "2.38"
           }
         ]
       }
@@ -233,19 +237,19 @@ export class StatsComponent implements OnInit {
         "data": [
             {
                 "label": "1 reaction",
-                "value": "1250400"
+                "value": "6680000"
             },
             {
                 "label": "2 reactions",
-                "value": "1463300"
+                "value": "2000000"
             },
             {
                 "label": "3 reactions",
-                "value": "1050700"
+                "value": "600001"
             },
             {
                 "label": "4 reactions",
-                "value": "491000"
+                "value": "600000"
             }
         ]
       }
